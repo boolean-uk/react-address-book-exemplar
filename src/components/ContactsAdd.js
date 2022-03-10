a9c383f1e5ca77891725deb07175c6891720ee8f
Lewis <ljoncb@gmail.com>
2022-03-10 15:43:06 +0000

    core criteria

--- src/components/ContactsAdd.js
+++ src/components/ContactsAdd.js
@@ -1,31 +1,57 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom";
 
+const initialNewContact = {
+  firstName: '',
+  lastName: '',
+  street: '',
+  city: '',
+}
+
 function ContactsAdd(props) {
-
-  // setContacts and contacts must be passed as props
-  // to this component so new contacts can be added to the
-  // state
   const { setContacts, contacts } = props
+  const [newContact, setNewContact] = useState(initialNewContact)
 
-  //TODO: Implement controlled form
-  //send POST to json server on form submit
+  const onFormSubmit = (event) => {
+    event.preventDefault()
+    fetch('http://localhost:4000/contacts', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newContact)
+    }).then(res => res.json())
+    .then((result) => {
+      setContacts([...contacts, result])
+      event.target.reset()
+      setNewContact(initialNewContact)
+    })
+  }
+
+  const updateContact = (property, value) => {
+    setNewContact(prev => {
+      const updated = {...prev}
+      updated[property] = value
+      return updated
+    })
+  }
 
   return (
-    <form className="form-stack contact-form">
+    <form className="form-stack contact-form" onSubmit={onFormSubmit}>
       <h2>Create Contact</h2>
 
       <label htmlFor="firstName">First Name</label>
-      <input id="firstName" name="firstName" type="text" required />
+      <input id="firstName" name="firstName" type="text" required onChange={(e) => updateContact('firstName', e.target.value)} />
 
       <label htmlFor="lastName">Last Name:</label>
-      <input id="lastName" name="lastName" type="text" required/>
+      <input id="lastName" name="lastName" type="text" required onChange={(e) => updateContact('lastName', e.target.value)}/>
 
       <label htmlFor="street">Street:</label>
-      <input id="street" name="street" type="text" required/>
+      <input id="street" name="street" type="text" required onChange={(e) => updateContact('street', e.target.value)}/>
 
       <label htmlFor="city">City:</label>
-      <input id="city" name="city" type="text" required/>
+      <input id="city" name="city" type="text" required onChange={(e) => updateContact('city', e.target.value)}/>
 
       <div className="actions-section">
         <button className="button blue" type="submit">
--- src/components/ContactsView.js
+++ src/components/ContactsView.js
@@ -3,10 +3,14 @@ import { useParams } from "react-router-dom"
 
 function ContactsView() {
   const [contact, setContact] = useState(false)
-
-  //TODO: Get the contact to load from the params and fetch.
-  //With useEffect, load the contact when params changes
-  //and update contact state
+  const params = useParams()
+  useEffect(() => {
+    fetch(`http://localhost:4000/contacts/${params.id}`)
+    .then(res => res.json())
+    .then(res => {
+      setContact(res)
+    })
+  }, [params])
 
   if (!contact) {
     return <p>Loading</p>
